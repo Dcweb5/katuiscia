3002d28a9e9f0f59d7a60eaef89b916302f4314c
Divo455 <divodjoko@gmail.com>
2026-05-23 00:51:19 +0100

    Fix: bypass SSL in local dev for Gemini API

--- app/Http/Controllers/ChatbotController.php
+++ app/Http/Controllers/ChatbotController.php
@@ -25,6 +25,7 @@ class ChatbotController extends Controller
 
         try {
             $response = Http::timeout(15)
+                ->withOptions(['verify' => !app()->isLocal()])
                 ->post('https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash:generateContent?key=' . $apiKey, [
                     'contents' => [[
                         'parts' => [
